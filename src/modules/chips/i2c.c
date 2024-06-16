4970449d6571e59d987e2a0f959f67b536fa8bff
Max D <max.davletchin@gmail.com>
2024-06-16 21:57:50 +0300

    Реализация компонента i2c.h

---
+++ src/modules/chips/i2c.c
@@ -0,0 +1,351 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "../../log.h"
+#include "i2c.h"
+
+// TEMP: Закомментированы библиотеки i2c для Linux
+
+#ifdef __linux__
+
+//#include <linux/i2c-dev.h>
+
+/// @brief Системные подключения I2C
+static const char* I2C_LINUX_DEVICES[4] = {
+    "/dev/i2c-0",
+    "/dev/i2c-1",
+    "/dev/i2c-2",
+    "/dev/i2c-3"
+};
+
+#endif
+
+/// @brief Частота I2C по умолчанию
+static const int I2C_DEFAULT_FREQUENCY = 100000;
+
+/// @brief Время ожидания при чтении, в миллисекундах
+static const int I2C_DEFAULT_READ_TIMEOUT_MS = 100;
+
+/// @brief Прочитать из слейва I2C данные (max 2 bytes)
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param regAddress Стартовый регистр
+/// @param regsCount Количество регистров (если более 2, то считаем, что это машинное слово)
+/// @return Числовое значение в байтах (от 0x0000 до 0xffff). Отрицательное значение - ошибка
+static int I2C_Read(I2C_Connection *connection, uint16_t address, uint16_t regAddress, uint8_t regsCount)
+{
+    int result = -1;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+
+    if (regsCount > 2)
+        regsCount = 2;
+
+#ifdef __linux__
+    // Выбор устройства на шине
+    int res = ioctl(connection->fd, I2C_SLAVE, address)
+    if (res < 0) 
+    {
+        Log_Write("I2C: ERROR. Failed to acquire bus access and/or find the slave %d (0x%02x)!", address, address);
+        return result;
+    }
+
+    // Выбор стартового регистора для чтения
+    res = write(connection->fd, regAddress, 1);
+    if (res != regsCount)
+    {
+        Log_Write("I2C: ERROR. Faild to use start register 0x%02x (count = %d) for slave %d (0x%02x)!", 
+            regAddress, regsCount, address, address);
+        return result;
+    }
+
+    // Чтение данных из регистра
+    res = read(connection->fd, &result, regsCount);
+    if (res != regsCount)
+    {
+        Log_Write("I2C: ERROR. The slave %d (0x%02x) is not responding!", address, address);
+        result = -1;
+        return result;
+    }
+#else
+    Log_Write("I2C: ERROR. Reading data from I2C is not supported on this platform!");
+    return result;
+#endif
+
+    return result;
+}
+
+
+/// @brief Записать в слейв I2C данные
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param regAddress Адрес стартового регистра для записи
+/// @param data Данные для записи
+/// @param len Длина данных
+/// @return Результат: true - успех, false - ошибка
+static bool I2C_Write(I2C_Connection *connection, uint16_t address, uint16_t regAddress, uint8_t* data, int len)
+{
+    bool result = false;
+    if (connection == NULL)
+        return result;
+    // TODO: Ready to test
+#ifdef __linux__
+    // Создание буфера для записи
+    uint8_t buffer[len + 1]; // Первый байт - адрес регистра
+    for (size_t i = 0; i < len; i++) {
+        buffer[i + 1] = data[i];
+    }
+
+    // Выбор устройства на шине
+    int res = ioctl(connection->fd, I2C_SLAVE, address)
+    if (res < 0) 
+    {
+        Log_Write("I2C: ERROR. Failed to acquire bus access and/or find the slave %d (0x%02x)!", address, address);
+        return result;
+    }
+
+    // Запись данных в регистр
+    res = write(connection->fd, buffer, len + 1);
+    if (res != regsCount)
+    {
+        Log_Write("I2C: ERROR. Faild to use start register 0x%02x (count = %d) for slave %d (0x%02x)!", 
+            regAddress, regsCount, address, address);
+        return result;
+    }
+#else
+    Log_Write("I2C: ERROR. Writing data to I2C is not supported on this platform!");
+    return result;
+#endif
+
+    result = true;
+    return result;
+}
+
+
+// ***** Предоставляемые интерфейсы i2c.h (реализация)*****
+
+I2C_Connection* I2C_CreateConnection(I2C_Config *config)
+{
+    I2C_Connection* connection = NULL;
+    // TODO: Ready to test
+    if (config == NULL)
+        return connection;
+
+#ifdef __linux__
+    connection = calloc(1, sizeof(I2C_Connection));
+
+    if (connection == NULL)
+    {
+        Log_Write("I2C: ERROR. Failed to create I2C connection instance!");
+        return connection;
+    }
+
+    connection->opened = false;
+
+    // Открытие устройства на шине
+    connection->fd = open(I2C_LINUX_DEVICES[config->unitNumber], O_RDWR);
+    if (connection->fd < 0)
+    {
+        Log_Write("I2C: ERROR. Failed to open I2C bus! It may be missing, unavailable or busy!");
+        I2C_DestroyConnection(connection);
+        return connection;
+    }   
+
+    // Установка частоты
+    bool res = I2C_SetFrequency(connection, config->frequencyHz);
+    if (res == false)
+    {
+        Log_Write("I2C: WARNING. Failed to set new frequency (%d Hz) of I2C!", config->frequencyHz);
+        //I2C_DestroyConnection(connection);
+        //return connection;
+    }
+
+    // Установка таймаута чтения
+    res = I2C_SetReadTimeout(connection, config->readTimeoutMs);
+    if (res == false)
+    {
+        Log_Write("I2C: WARNING. Failed to set new read timeout (%d ms) of I2C!", config->readTimeoutMs);
+        //I2C_DestroyConnection(connection);
+        //return connection;
+    }
+
+    connection->opened = true;    
+#else
+    Log_Write("I2C: ERROR. Create connection to I2C is not supported on this platform!");
+    return connection;
+#endif
+
+    connection->opened = true;
+
+    return connection;
+}
+
+
+bool I2C_DestroyConnection(I2C_Connection *connection)
+{
+    int result = false;
+    // TODO: Ready to test
+    if (connection != NULL)
+    {
+        if (connection->opened)
+        {
+            close(connection->fd);            
+        }
+        free(connection);
+        connection = NULL;
+    }
+
+    result = true;
+    return result;
+}
+
+
+bool I2C_CheckConnectionAddress(I2C_Connection *connection, uint16_t address)
+{
+    bool result = false;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;    
+
+#ifdef __linux__
+    // Проверка наличия устройства на шине (через установку адреса устройства)
+    int res = ioctl(connection->fd, I2C_SLAVE, address);
+    if (res < 0) 
+    {
+        Log_Write("I2C: ERROR. Failed to acquire bus access and/or find the slave %d (0x%02x)!", address, address);
+        return result;
+    }
+
+    // Попробуем выполнить чтение одного байта для проверки устройства
+    char buffer;
+    int res = read(connection->fd, &buffer, 1);
+    if (res != 1)
+    {
+        Log_Write("I2C: ERROR. The slave %d (0x%02x) is not responding!", address, address);
+        return result;
+    }
+
+#else
+    Log_Write("I2C: ERROR. Check connection to I2C is not supported on this platform!");
+    return result;
+#endif
+
+    result = true;
+    return result;
+}
+
+
+bool I2C_SetFrequency(I2C_Connection *connection, int frequencyHz)
+{
+    bool result = false;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+
+    if (frequencyHz <= 0)
+    {
+        frequencyHz = I2C_DEFAULT_FREQUENCY;
+    }
+
+#ifdef __linux__
+    // !!! Не уверен в правильности и последовательности действий !!!
+    // Установка частоты
+    //int i2cFrequencyCode = i2c_frequency_to_code(frequencyHz);  // Функция для преобразования частоты в код частоты
+    int res = ioctl(connection->fd, I2C_FREQ, i2cFrequencyCode);
+    if (res < 0)
+    {
+        Log_Write("I2C: ERROR. Failed to set I2C frequency to %d Hz!", frequencyHz);
+    }
+#else
+    Log_Write("I2C: ERROR. Set frequency to I2C is not supported on this platform!");
+    return result;
+#endif
+
+    result = true;
+    return result;
+}
+
+
+bool I2C_SetReadTimeout(I2C_Connection *connection, int readTimeoutMs)
+{
+    bool result = false;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+
+    if (readTimeoutMs <= 0)
+        readTimeoutMs = I2C_DEFAULT_READ_TIMEOUT_MS;
+
+#ifdef __linux__
+    // !!! Не уверен в правильности и последовательности действий !!!
+    // Установка таймаута чтения
+    int res = ioctl(connection->fd, I2C_TIMEOUT, readTimeoutMs);
+    if (res < 0)
+    {
+        Log_Write("I2C: ERROR. Failed to set I2C read timeout to %d ms!", readTimeoutMs);
+    }
+#else
+    Log_Write("I2C: ERROR. Set read timeout to I2C is not supported on this platform!");
+    return result;
+#endif
+
+    result = true;
+    return result;
+}
+
+
+int I2C_ReadRegByte(I2C_Connection *connection, uint16_t address, uint16_t regAddress)
+{
+    int result = -1;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+    
+    result = I2C_Read(connection, address, regAddress, 1);
+
+    return result;
+}
+
+
+int I2C_ReadRegWord(I2C_Connection *connection, uint16_t address, uint16_t regAddress)
+{
+    int result = -1;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+    
+    result = I2C_Read(connection, address, regAddress, 2);
+
+    return result;
+}
+
+
+bool I2C_WriteRegByte(I2C_Connection *connection, uint16_t address, uint16_t regAddress, uint8_t regValue)
+{
+    bool result = false;
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+
+    result = I2C_Write(connection, address, regAddress, &regValue, 1);
+
+    return result;
+}
+
+
+bool I2C_WriteRegWord(I2C_Connection *connection, uint16_t address, uint16_t regAddress, uint16_t regValue)
+{
+    bool result = false;
+    uint8_t data[2];
+    // TODO: Ready to test
+    if (connection == NULL)
+        return result;
+
+    data[0] = (regValue >> 8) & 0xff;
+    data[1] = regValue & 0xff;
+
+    result = I2C_Write(connection, address, regAddress, data, 2);
+
+    return result;
+}
---
+++ src/modules/chips/i2c.h
@@ -0,0 +1,104 @@
+/*
+Библиотека I2C.
+*/
+
+#ifndef IC2_H
+#define IC2_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+// Конфигурация I2C
+typedef struct
+{
+    uint8_t unitNumber;         // Номер шины в системе
+    int frequencyHz;              // Частота, Гц
+    int readTimeoutMs;          // Таймаут при чтении, мс
+} I2C_Config;
+
+// Подключение I2C
+typedef struct
+{
+    bool opened;            // Подключение открыто
+    int fd;                 // Файловый дескриптор I2C
+    I2C_Config *config;     // Конфигурация
+} I2C_Connection;
+
+/// @brief Создать экземпляр I2C подключения. Подключение остается открытым
+/// @param config Конфигурация
+/// @return Экземпляр подключения
+I2C_Connection* I2C_CreateConnection(I2C_Config *config);
+
+/// @brief Удалить экземпляр I2C подключения. Подключение закрывается
+/// @param connection Подключение
+/// @return Результат: true - успешно, false - ошибка
+bool I2C_DestroyConnection(I2C_Connection *connection);
+
+/// @brief Проверить связь с устройством на шине I2C по адресу
+/// @param connection Подключение
+/// @param address Адрес устройства
+/// @return Результат: true - успешно, false - ошибка
+bool I2C_CheckConnectionAddress(I2C_Connection *connection, uint16_t address);
+
+/// @brief Установить частоту I2C
+/// @param connection Подключение
+/// @param frequency Частота
+/// @return Результат: true - успешно, false - ошибка
+bool I2C_SetFrequency(I2C_Connection *connection, int frequency);
+
+/// @brief Установить таймаут при чтении
+/// @param connection Подключение
+/// @param readTimeoutMs Таймаут, мс
+/// @return Результат: true - успешно, false - ошибка
+bool I2C_SetReadTimeout(I2C_Connection *connection, int readTimeoutMs);
+
+/// @brief Прочитать байт по I2C
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param regAddress Адрес регистра
+/// @param regValue Значение регистра
+/// @return Числовое значение в 1 байт (от 0x00 до 0xff). Отрицательное значение - ошибка
+int I2C_ReadRegByte(I2C_Connection *connection, uint16_t address, uint16_t regAddress);
+
+/// @brief Прочитать машинное слово по I2C
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param regAddress Адрес регистра
+/// @param regsValue Значение регистров
+/// @return Числовое значение в 2 байта (от 0x0000 до 0xffff). Отрицательное значение - ошибка
+int I2C_ReadRegWord(I2C_Connection *connection, uint16_t address, uint16_t regAddress);
+
+/// @brief Чтение данных из I2C
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param buffer Буфер данных (отпавляемые)
+/// @param len Длина буфера данных
+/// @return Результат операции (Код ошибки)
+//int I2C_Read(I2C_Connection *connection, uint16_t address, uint8_t* buffer, int len);
+
+/// @brief Запись одного байта в I2C
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param regAddress Адрес регистра
+/// @param regValue Значение регистра
+/// @return Результат: true - успешно, false - ошибка
+bool I2C_WriteRegByte(I2C_Connection *connection, uint16_t address, uint16_t regAddress, uint8_t regValue);
+
+/// @brief Запись машинного слова в I2C
+/// @param connection Подключение
+/// @param address Адрес устройства на шине
+/// @param regAddress Адрес регистра
+/// @param regValue Значение регистров
+/// @return Результат: true - успешно, false - ошибка
+bool I2C_WriteRegWord(I2C_Connection *connection, uint16_t address, uint16_t regAddress, uint16_t regValue);
+
+/// @brief Запись данных в I2C
+/// @param connection Подключение
+/// @param address Адрес слейва на шине
+/// @param buffer Буфер данных для записи
+/// @param len Длина буфера данных 
+/// @return Результат: true - успешно, false - ошибка
+//bool I2C_Write(I2C_Connection *connection, uint16_t address, uint8_t* buffer, int len);
+
+
+#endif
